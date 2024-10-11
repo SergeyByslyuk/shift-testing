4034d55c5e5556119fbc13a9d098a9c206fb4fbc
busljuk <s.busljuk@kpedev.by>
2024-10-11 21:53:25 +0300

    refactoring

--- tdd-example/src/test/java/OrderServiceTest.java
+++ tdd-example/src/test/java/OrderServiceTest.java
@@ -4,19 +4,14 @@ import org.kpe.Order;
 import org.kpe.OrderService;
 import org.kpe.Product;
 import org.kpe.User;
-import org.kpe.UserRepo;
 import org.kpe.UserRepoImpl;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoMoreInteractions;
-import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class OrderServiceTest {
